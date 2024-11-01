8c5ec54bbcda756cb358a35f84ab2f2d736a3148
David Iraheta <david.iraheta.89@gmail.com>
2024-11-01 16:24:19 -0600

    Python201.11.1

--- 02_more-datatypes/02_CLIRPG2.py
+++ 02_more-datatypes/02_CLIRPG2.py
@@ -10,53 +10,114 @@
 # This pseudo-random element can have an effect on whether your player wins or loses when battling an opponent.
 
 import random  
-set_of_choices = {"left", "right", "look around", "fight", "leave", "inventory"}
-item_inventory= []
-user_name = input("What is your name?: ")
-print(f"Hello {user_name} Welcome to Medieval Times")
-print("There are two doors in front of you. One on the left and one on the right")
-has_sword = False
-has_inventory = True
-door_choice = input("Which door do you choose, left or right?: ").lower()
-if door_choice == "left":
-    print("You have entered an empty room")
-    look_around = input("Would you like to look around? yes / no: ").lower()
-    if look_around == "yes":
-        print("You have found a sword!")
-        sword_choice = input("Would you like to take the sword: yes / no: ").lower()
-        if sword_choice == "yes":
-            has_sword = True
-            print("You now have a sword!")
+set_of_choices = set("left, right, far right, look around, fight, leave, inventory")
+inventory= []
+user_attack = random.randint(4, 8)
+user_defense = 8
+user_health = 10
+goblin_attack = random.randint(1, 3)
+goblin_defense = 4
+goblin_health = 2
+ogre_attack = random.randint(3, 8)
+ogre_defense = 12
+ogre_health = 6
+dragon_attack = random.randint(10, 12)
+dragon_defense = 20
+dragon_health = 10
+sword = inventory.append("sword")
+golden_axe = inventory.append("golden axe")
+shield = inventory.append("shield")
+potion = inventory.append("potion")
+user_name = input("To get started, please enter your name: ")
+print(f"Hello {user_name} Welcome to Echoes of the Keep!")
+door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+while user_health > 0:
+     
+    if door_choice == "left":
+        print("You have entered an empty-looking room")
+        look_around = input("Would you like to look around? yes / no: ").lower()
+        if look_around == "yes":
+            print("You have found a secret latch in the floor if the room. You open the latch, it contains a sword!")
+            sword_choice = input("Would you like to take the sword: yes / no: ").lower()
+            if sword_choice == "yes":
+                inventory.append(sword)
+                print("You now have a sword!")
+                print("You have returned to the castle lobby")
+                print(inventory)
+                input("Would you like to use an item from your inventory? yes / no: ").lower()
+                input("Which item would you like to use?").lower()
+                inventory.remove()
+            else: 
+                print("You have left the sword")
         else: 
-            print("You have left the sword")
-    else: 
-        print("You have left the room without looking around")
-    open_right_door  = input("Would you like to open the right door now? yes / no: ").lower()
-    if open_right_door == "yes":
+            print("You have left the room without looking around")
+            print("You have returned to the castle lobby")
+            print(inventory)
+            input("Would you like to use an item from your inventory? yes / no: ").lower()
+            input("Which item would you like to use?").lower()
+            door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+
+    if door_choice == "far left":
+        print("You have entered the room with a chest!")
+        print("You walk towards the chest and an ogre jumps out and charges at you!")
+        print("You attack the ogre!. You dodge its blow and strike first. ")
+        ogre_health -= user_attack
+        print(f"The ogre has {ogre_health} health left.")
+        print("The ogre attacks you! You block the blow and counterattack!")
+        user_health -= ogre_attack
+        print(f"You have {user_health} health left.")
+        print("You attack the ogre!")
+        ogre_health -= user_attack
+        print("You have defeated the ogre!")
+        print("You open the chest and find a potion!")
+        print("You have added the potion to your inventory!")
+        inventory.append(potion)
+        print("You have returned to the castle lobby")
+        print(inventory)
+        door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+    if door_choice == " far right":
+        print(" You have entered a room with nothing but two armoirs in the corner")
+    open_large_armoir = input("You have entered a room with nothing but two armoirs in the corner. Would you like to open the large armoir? yes / no: ").lower()
+    if open_large_armoir == "yes":
+            print("You have found a golden axe!")
+            axe_choice = input("Would you like to take the golden axe? yes / no: ").lower()
+            if axe_choice == "yes":
+                inventory.append(golden_axe)
+                print("You now have a golden axe!")
+            else: 
+                print("You have left the axe")
+    open_small_armoir = input("Would you like to open the small armoir? yes / no: ").lower()
+    if open_small_armoir == "yes":
+            print("A goblin attacks!")
+            print(f"You attack the goblin!. You dodge its blow and strike first. You hit the goblin for 5 damage!")
+            goblin_health -= user_attack
+            print("You have defeated the goblin!")
+            dagger_choice = input("The goblin dropped a rusty dagger. Would you like to like to add the rusty dagger to inventory? yes / no: ")
+    if dagger_choice == "yes":
+        inventory.append("dagger")
+        print("You now have a rusty dagger!")
+    else:
+        print("You have left the rusty dagger") 
+    print("You have returned to the castle lobby")
+    print(inventory)
+    input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+    
+    if door_choice == "right":
         print("You have entered the room with a dragon!")
         fight_choice = input("Would you like to fight the dragon? yes / no: ").lower()
         if fight_choice == "yes":
-            if has_sword:
-                print("You bravely fight the dragon with your sword and win! You are victorious!")
+            if golden_axe in inventory:
+                print("You bravely fight the dragon with your axe and win! You are victorious!")
             else:
-                print("You try to fight the dragon, but without a sword, you are eaten. Game over.")
+                print("You try to fight the dragon, but without a golden axe, you are eaten. Game over.")
+        elif fight_choice == "no":
+            has_inventory = False
+            print("You chose not to fight the dragon. You lose.")
+
         else:
             print("You chose not to fight the dragon. You lose.")
-elif door_choice == "right":
-    print("You have entered the room with a dragon!")
-    fight_choice = input("Would you like to fight the dragon? yes / no: ").lower()
-    if fight_choice == "yes":
-        if has_sword:
-            print("You bravely fight the dragon with your sword and win! You are victorious!")
-        else:
-            print("You try to fight the dragon, but without a sword, you are eaten. Game over.")
-    elif fight_choice == "no":
-        has_inventory = False
-        print("You chose not to fight the dragon. You lose.")
+    else: 
+        print("Invalid choice. You lose.")
 
-    else:
-        print("You chose not to fight the dragon. You lose.")
-else: 
-    print("Invalid choice. You lose.")
 
-    
+        
--- projects/file_counter.py
+++ projects/file_counter.py
@@ -1,1 +1,33 @@
 # Add the code for the file counter script that you wrote in the course.
+# In the previous module of this course, you wrote a script that was able to move all your screenshots from your 
+# messy desktop into a new folder. In this project, you'll revisit your file mover code and expand on it 
+# with the knowledge of some additional data types in order to make it more flexible and powerful.
+# When you built your file mover, you used the pathlib module that worked with Path() objects. 
+# You can consider Path() objects as yet another data type, one that is more custom and not considered a built-in
+# type. As you might remember, you'll have to import it from the standard library in order to work with it.
+# In this project, you'll continue to work with pathlib and files, 
+# but you'll also use some of the additional built-in data types that you got to know in this section.
+
+from pathlib import Path
+file_count = {}
+desktop = Path.home() / "Desktop"
+screenshot_folder = desktop / "Screenshots"
+screenshot_folder.mkdir(exist_ok=True)
+for file in desktop.iterdir():
+    
+    if file.is_file():
+      
+        file_extension = file.suffix.lower()
+        if file_extension in file_count:
+            file_count[file_extension] += 1
+        else:
+            file_count[file_extension] = 1
+        if "screenshot" in file.stem.lower():
+            destination = screenshot_folder / file.name
+            file.rename(destination)
+        if "screenshot" in file.stem.lower():
+            destination = screenshot_folder / file.name
+            file.rename(destination)
+for ext, count in file_count.items():
+    print(f"There are {count} file(s) with the '{ext}' extension on your desktop.")
+
