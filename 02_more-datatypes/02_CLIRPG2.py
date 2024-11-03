3bd38bc055f444fd0f071ba8403a466f120412f8
David Iraheta <david.iraheta.89@gmail.com>
2024-11-03 11:08:51 -0700

    Python201.11.3

--- 02_more-datatypes/02_CLIRPG2.py
+++ 02_more-datatypes/02_CLIRPG2.py
@@ -9,115 +9,189 @@
 # Use the random module to add a multiplier to your battles, similar to a dice roll in a real game.
 # This pseudo-random element can have an effect on whether your player wins or loses when battling an opponent.
 
-import random  
-set_of_choices = set("left, right, far right, look around, fight, leave, inventory")
-inventory= []
-user_attack = random.randint(4, 8)
-user_defense = 8
-user_health = 10
-goblin_attack = random.randint(1, 3)
-goblin_defense = 4
-goblin_health = 2
-ogre_attack = random.randint(3, 8)
-ogre_defense = 12
-ogre_health = 6
-dragon_attack = random.randint(10, 12)
-dragon_defense = 20
-dragon_health = 10
-sword = inventory.append("sword")
-golden_axe = inventory.append("golden axe")
-shield = inventory.append("shield")
-potion = inventory.append("potion")
-user_name = input("To get started, please enter your name: ")
-print(f"Hello {user_name} Welcome to Echoes of the Keep!")
-door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
-while user_health > 0:
+# import random  
+# set_of_choices = set(["left", "right", "far right", "look around", "fight", "leave", "inventory"])
+# inventory= []
+# user_attack = random.randint(4, 8)
+# user_defense = 8
+# user_health = 10
+# goblin_attack = random.randint(1, 3)
+# goblin_defense = 4
+# goblin_health = 2
+# ogre_attack = random.randint(3, 8)
+# ogre_defense = 12
+# ogre_health = 6
+# dragon_attack = random.randint(10, 12)
+# dragon_defense = 20
+# dragon_health = 10
+# items = {"sword": 5, "potion": 2, "golden_axe": 8, "rusty_dagger": 3}
+# user_name = input("To get started, please enter your name: ")
+# print(f"Hello {user_name} Welcome to Echoes of the Keep!")
+# door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+# while user_health > 0:
      
-    if door_choice == "left":
-        print("You have entered an empty-looking room")
-        look_around = input("Would you like to look around? yes / no: ").lower()
-        if look_around == "yes":
-            print("You have found a secret latch in the floor if the room. You open the latch, it contains a sword!")
-            sword_choice = input("Would you like to take the sword: yes / no: ").lower()
-            if sword_choice == "yes":
-                inventory.append(sword)
-                print("You now have a sword!")
-                print("You have returned to the castle lobby")
-                print(inventory)
-                input("Would you like to use an item from your inventory? yes / no: ").lower()
-                input("Which item would you like to use?").lower()
-                inventory.remove()
-            else: 
-                print("You have left the sword")
-        else: 
-            print("You have left the room without looking around")
-            print("You have returned to the castle lobby")
-            print(inventory)
-            input("Would you like to use an item from your inventory? yes / no: ").lower()
-            input("Which item would you like to use?").lower()
-            door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+#     if door_choice == "left":
+#         print("You have entered an empty-looking room")
+#         look_around = input("Would you like to look around? yes / no: ").lower()
+#         if look_around == "yes":
+#             print("You have found a secret latch in the floor if the room. You open the latch, it contains a sword!")
+#             sword_choice = input("Would you like to take the sword: yes / no: ").lower()
+#             if sword_choice == "yes":
+#                 inventory.append(sword)
+#                 print("You now have a sword!")
+#                 print("You have returned to the castle lobby")
+#                 print(inventory)
+#                 input("Would you like to use an item from your inventory? yes / no: ").lower()
+#                 input("Which item would you like to use?").lower()
+#                 inventory.remove()
+#             else: 
+#                 print("You have left the sword")
+#         else: 
+#             print("You have left the room without looking around")
+#             print("You have returned to the castle lobby")
+#             print(inventory)
+#             input("Would you like to use an item from your inventory? yes / no: ").lower()
+#             input("Which item would you like to use?").lower()
+#             door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
 
-    if door_choice == "far left":
-        print("You have entered the room with a chest!")
-        print("You walk towards the chest and an ogre jumps out and charges at you!")
-        print("You attack the ogre!. You dodge its blow and strike first. ")
-        ogre_health -= user_attack
-        print(f"The ogre has {ogre_health} health left.")
-        print("The ogre attacks you! You block the blow and counterattack!")
-        user_health -= ogre_attack
-        print(f"You have {user_health} health left.")
-        print("You attack the ogre!")
-        ogre_health -= user_attack
-        print("You have defeated the ogre!")
-        print("You open the chest and find a potion!")
-        print("You have added the potion to your inventory!")
-        inventory.append(potion)
-        print("You have returned to the castle lobby")
-        print(inventory)
-        door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
-    if door_choice == " far right":
-        print(" You have entered a room with nothing but two armoirs in the corner")
-    open_large_armoir = input("You have entered a room with nothing but two armoirs in the corner. Would you like to open the large armoir? yes / no: ").lower()
-    if open_large_armoir == "yes":
-            print("You have found a golden axe!")
-            axe_choice = input("Would you like to take the golden axe? yes / no: ").lower()
-            if axe_choice == "yes":
-                inventory.append(golden_axe)
-                print("You now have a golden axe!")
-            else: 
-                print("You have left the axe")
-    open_small_armoir = input("Would you like to open the small armoir? yes / no: ").lower()
-    if open_small_armoir == "yes":
-            print("A goblin attacks!")
-            print(f"You attack the goblin!. You dodge its blow and strike first. You hit the goblin for 5 damage!")
-            goblin_health -= user_attack
-            print("You have defeated the goblin!")
-            dagger_choice = input("The goblin dropped a rusty dagger. Would you like to like to add the rusty dagger to inventory? yes / no: ")
-    if dagger_choice == "yes":
-        inventory.append("dagger")
-        print("You now have a rusty dagger!")
-    else:
-        print("You have left the rusty dagger") 
-    print("You have returned to the castle lobby")
-    print(inventory)
-    input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+#     elif door_choice == "far left":
+#         print("You have entered the room with a chest!")
+#         print("You walk towards the chest and an ogre jumps out and charges at you!")
+#         print("You attack the ogre!. You dodge its blow and strike first. ")
+#         ogre_health -= user_attack
+#         print(f"The ogre has {ogre_health} health left.")
+#         print("The ogre attacks you! You block the blow and counterattack!")
+#         user_health -= ogre_attack
+#         print(f"You have {user_health} health left.")
+#         print("You attack the ogre!")
+#         ogre_health -= user_attack
+#         print("You have defeated the ogre!")
+#         print("You open the chest and find a potion!")
+#         print("You have added the potion to your inventory!")
+#         inventory.append(potion)
+#         print("You have returned to the castle lobby")
+#         print(inventory)
+#         door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+#     elif door_choice == " far right":
+#         print(" You have entered a room with nothing but two armoirs in the corner")
+#     open_large_armoir = input("You have entered a room with nothing but two armoirs in the corner. Would you like to open the large armoir? yes / no: ").lower()
+#     if open_large_armoir == "yes":
+#             print("You have found a golden axe!")
+#             axe_choice = input("Would you like to take the golden axe? yes / no: ").lower()
+#             if axe_choice == "yes":
+#                 inventory.append(golden_axe)
+#                 print("You now have a golden axe!")
+#             else: 
+#                 print("You have left the axe")
+#     open_small_armoir = input("Would you like to open the small armoir? yes / no: ").lower()
+#     if open_small_armoir == "yes":
+#             print("A goblin attacks!")
+#             print(f"You attack the goblin!. You dodge its blow and strike first. You hit the goblin for 5 damage!")
+#             goblin_health -= user_attack
+#             print("You have defeated the goblin!")
+#     dagger_choice = input("The goblin dropped a rusty dagger. Would you like to like to add the rusty dagger to inventory? yes / no: ")
+#     if dagger_choice == "yes":
+#         inventory.append("dagger")
+#         print("You now have a rusty dagger!")
+#     else:
+#         print("You have left the rusty dagger") 
+#     print("You have returned to the castle lobby")
+#     print(inventory)
+#     input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
     
-    if door_choice == "right":
-        print("You have entered the room with a dragon!")
-        fight_choice = input("Would you like to fight the dragon? yes / no: ").lower()
-        if fight_choice == "yes":
-            if golden_axe in inventory:
-                print("You bravely fight the dragon with your axe and win! You are victorious!")
-            else:
-                print("You try to fight the dragon, but without a golden axe, you are eaten. Game over.")
-        elif fight_choice == "no":
-            has_inventory = False
-            print("You chose not to fight the dragon. You lose.")
+#     elif door_choice == "right":
+#     print("You have entered the room with a dragon!")
+#     fight_choice = input("Would you like to fight the dragon? yes / no: ").lower()
+#     if fight_choice == "yes":
+#     if golden_axe in inventory:
+#                 print("You bravely fight the dragon with your axe and win! You are victorious!")
+#         else:
+#                 print("You try to fight the dragon, but without a golden axe, you are eaten. Game over.")
+#     elif fight_choice == "no":
+#             has_inventory = False
+#             print("You chose not to fight the dragon. You lose.")
 
-        else:
-            print("You chose not to fight the dragon. You lose.")
-    else: 
-        print("Invalid choice. You lose.")
+#         else:
+#             print("You chose not to fight the dragon. You lose.")
+#     else: 
+#         print("Invalid choice. You lose.")
 
 
         
+import random
+
+# Define possible choices
+set_of_choices = set(["left", "right", "far left", "far right", "look around", "fight", "leave", "inventory"])
+inventory = []
+
+# Define items with integer values for their effect (e.g., attack or defense power)
+items = {"sword": 5, "golden axe": 10, "shield": 3,"potion": 15}
+# Player stats
+user_attack = random.randint(4, 8)
+user_defense = 8
+user_health = 10
+# Enemy stats
+goblin_attack = random.randint(1, 3)
+goblin_defense = 4
+goblin_health = 2
+
+user_name = input("To get started, please enter your name: ")
+print(f"Hello {user_name}! Welcome to Echoes of the Keep!")
+door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
+
+while user_health > 0:
+    if door_choice == "left":
+        print("You have entered an empty-looking room.")
+        look_around = input("Would you like to look around? yes / no: ").lower()
+        if look_around == "yes":
+            print("You have found a secret latch containing a sword!")
+            sword_choice = input("Would you like to take the sword? yes / no: ").lower()
+            if sword_choice == "yes":
+                inventory.append("sword")
+                print(f"You now have a sword! (Attack Power: {items['sword']})")
+            else:
+                print("You have left the sword.")
+        print("You have returned to the castle lobby.")
+
+    elif door_choice == "far left":
+        print("You enter a room with a chest and are ambushed by an ogre!")
+        user_health -= goblin_attack
+        if user_health > 0:
+            print("You defeated the ogre and found a potion in the chest.")
+            inventory.append("potion")
+            print(f"You now have a potion! (Health Boost: {items['potion']})")
+        print("You have returned to the castle lobby.")
+
+    elif door_choice == "far right":
+        print("You enter a room with two armoires.")
+        open_large_armoire = input("Open the large armoire? yes / no: ").lower()
+        if open_large_armoire == "yes":
+            print("You have found a golden axe!")
+            if input("Take the golden axe? yes / no: ").lower() == "yes":
+                inventory.append("golden axe")
+                print(f"You now have a golden axe! (Attack Power: {items['golden axe']})")
+        open_small_armoire = input("Open the small armoire? yes / no: ").lower()
+        if open_small_armoire == "yes":
+            print("A goblin attacks!")
+            goblin_health -= user_attack
+            print("You defeated the goblin! It dropped a rusty dagger.")
+            if input("Take the dagger? yes / no: ").lower() == "yes":
+                inventory.append("dagger")
+                print("You now have a rusty dagger!")
+
+    elif door_choice == "right":
+        print("You have entered a room with a dragon!")
+        fight_choice = input("Would you like to fight the dragon? yes / no: ").lower()
+        if fight_choice == "yes":
+            if "golden axe" in inventory:
+                print(f"You bravely fight the dragon with your golden axe (Attack Power: {items['golden axe']}) and win! You are victorious!")
+            else:
+                print("Without a golden axe, you are defeated by the dragon. Game over.")
+                break
+        else:
+            print("You chose not to fight the dragon. You lose.")
+            break
+    else:
+        print("Invalid choice.")
+
+    door_choice = input("There are 4 doors in front of you. Choose one: left, right, far left, far right: ").lower()
--- projects/file_counter.py
+++ projects/file_counter.py
@@ -7,27 +7,57 @@
 # type. As you might remember, you'll have to import it from the standard library in order to work with it.
 # In this project, you'll continue to work with pathlib and files, 
 # but you'll also use some of the additional built-in data types that you got to know in this section.
-
 from pathlib import Path
+
+# Dictionary to count file types
 file_count = {}
+
+# Define paths
 desktop = Path.home() / "Desktop"
-screenshot_folder = desktop / "Screenshots"
-screenshot_folder.mkdir(exist_ok=True)
+screenshot_folder = desktop / "untitled folder"
+screenshot_folder.mkdir(exist_ok=True)  # Create Screenshots folder if it doesn't exist
+
+# Loop through each file on the desktop
 for file in desktop.iterdir():
-    
-    if file.is_file():
-      
+    if file.is_file():  # Only process files, not directories
         file_extension = file.suffix.lower()
-        if file_extension in file_count:
-            file_count[file_extension] += 1
-        else:
-            file_count[file_extension] = 1
+        
+        # Update file count dictionary
+        file_count[file_extension] = file_count.get(file_extension, 0) + 1
+        
+        # Move screenshots to the screenshot folder
         if "screenshot" in file.stem.lower():
             destination = screenshot_folder / file.name
+            if destination.exists():  # Handle duplicate files by renaming
+                destination = screenshot_folder / f"copy_of_{file.name}"
             file.rename(destination)
-        if "screenshot" in file.stem.lower():
-            destination = screenshot_folder / file.name
-            file.rename(destination)
+
+# Print the count of each file type
 for ext, count in file_count.items():
     print(f"There are {count} file(s) with the '{ext}' extension on your desktop.")
+
+# count = {'': 2, '.pdf': 2}
+# file_out = open("file_count.txt", "w")
+# file_out.write(str(count))
+# file_out.close()
+# # 'append' mode
+# file_out = open("filecounts.txt", "a")
+# file_out.write(str(count))
+# file_out.write("\n")  # Include a line break
+# file_out.close()
+# file_in = file_out = open("filecounts.txt", "r")
+# contents = file_in.read()
+# print(contents)
+# file_in.close()
+# with open("filecounts.txt", "r") as file_in:
+#     print(file_in.read())
+import csv
+# -- snip --
+count = {"": 8, ".csv": 2, ".md": 2, ".png": 11}
+
+with open("filecounts.csv", "a") as csvfile:
+    countwriter = csv.writer(csvfile)
+    data = [count[""], count[".csv"], count[".md"], count[".png"]]
+    countwriter.writerow(data)
+
 
